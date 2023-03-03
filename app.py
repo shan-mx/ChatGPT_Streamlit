84299e4a0305157630128c3415aa1586128964a1
Alex Shan <36291011+shan-mx@users.noreply.github.com>
2023-03-03 15:57:38 +0800

    feat: parameters

--- app.py
+++ app.py
@@ -5,28 +5,31 @@ st.title('GPT Assistant')
 user = "User" if "user" not in st.experimental_get_query_params() else st.experimental_get_query_params()["user"][0]
 initial_content = "" if "initial_content" not in st.session_state else st.session_state["initial_content"]
 api_key = "" if "api_key" not in st.session_state else st.session_state["api_key"]
-if user or 'data' not in st.session_state:
-    st.session_state['data'] = load_data(user, initial_content)
+
 if "rerun" not in st.session_state:
     st.session_state["rerun"] = True
 else:
     st.session_state.pop("rerun")
-chat_stats = st.session_state['data']["chat_stats"]
-chat_history = st.session_state['data']["chat_history"]
+
+st.session_state['data'] = load_data(user, initial_content)
+history, stats, paras = (value for key, value in st.session_state['data'].items())
+save_data(history, stats, paras, user)
+
 if user == "User":
     st.markdown("***\nIt seems like you're new to my chatGPT web client! Please create your own user profile in Settings.\n\n")
 if st.session_state["data"]:
-    show_messages(user, st.session_state['data']["chat_history"], st)
+    show_messages(user, st.session_state['data']["history"])
+
 with st.form("form", clear_on_submit=True):
     user_input = st.text_area(f"**{user}:**", key="input")
     if st.form_submit_button("Submit", use_container_width=True):
         openai.api_key = api_key if api_key != "" else st.secrets["apikey"]
-        chat_stats["Total Rounds"] += 1
+        stats["Total Rounds"] += 1
         t1 = time.time()
-        chat_history.append({"role": "user", "content": user_input})
+        history.append({"role": "user", "content": user_input})
         with st.spinner("Processing..."):
             try:
-                r = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=chat_history)
+                r = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=history, **paras)
             except openai.error.APIConnectionError:
                 return_text = "[Warning] API Connection Error, try again."
             except openai.error.InvalidRequestError:
@@ -35,45 +38,55 @@ with st.form("form", clear_on_submit=True):
                 return_text = "[Warning] Reached chat rate limit, do not send request too frequently."
             else:
                 return_text = r["choices"][0]["message"]["content"]
-                chat_stats["Total Tokens"] += r["usage"]["total_tokens"]
-                chat_stats["Tokens Used"] = r["usage"]["total_tokens"]
-            chat_history.append({"role": "assistant", "content": return_text})
-            chat_stats["Time Used"] = round(time.time() - t1, 3)
-            chat_stats["Total Time"] += chat_stats["Time Used"]
-            chat_stats["Averaged Time"] = round(chat_stats["Total Time"] / chat_stats["Total Rounds"], 3)
-            save_data(chat_history, chat_stats, user)
-            if "rerun" in st.session_state:
-                st.experimental_rerun()
+                stats["Total Tokens"] += r["usage"]["total_tokens"]
+                stats["Tokens Used"] = r["usage"]["total_tokens"]
+            stats["Time Used"] = round(time.time() - t1, 3)
+            stats["Total Time"] += stats["Time Used"]
+            stats["Averaged Time"] = round(stats["Total Time"] / stats["Total Rounds"], 3)
+            history.append({"role": "assistant", "content": return_text})
+            save_data(history, stats, paras, user)
+            st.experimental_rerun()
+
 with st.expander("Settings"):
     if st.button("Clear Chat History", use_container_width=True):
-        st.session_state['data']["chat_history"] = [{"role": "system", "content": initial_content}]
-        st.session_state['data']["chat_stats"] = initial_stats
-        save_data(st.session_state['data']["chat_history"], st.session_state['data']["chat_stats"], user)
+        st.session_state['data'] = initial_data(initial_content)
+        save_data(st.session_state['data']["history"], st.session_state['data']["stats"], st.session_state['data']["paras"], user)
         st.experimental_rerun()
     if user == "Admin":
-        users_list = [json_file.split("_")[-1].split(".")[0] for json_file in glob.glob(os.getcwd() + '/*.json')]
-        user_box = st.selectbox("All Users", users_list if "Admin" in users_list else ["Admin"] + users_list)
+        user_box = st.selectbox("All Users", [json_file.split("_")[-1].split(".")[0] for json_file in glob.glob(os.getcwd() + '/*.json')])
     else:
         user_box = st.text_input("Username (each user have their own chat data)", value=user)
     initial_content_box = st.text_input("Initial Instruction (applies when the chat history is cleared)", value=initial_content)
     api_key_box = st.text_input("OpenAI API Key (optional)", value=api_key)
-    if user_box:
+    if user_box and "rerun" in st.session_state:
         st.experimental_set_query_params(user=user_box)
-        if "rerun" in st.session_state:
-            st.experimental_rerun()
-    if initial_content_box:
+        st.experimental_rerun()
+    if initial_content_box and "rerun" in st.session_state:
         st.session_state["initial_content"] = initial_content_box
-        if "rerun" in st.session_state:
-            st.experimental_rerun()
-    if api_key_box:
+        st.experimental_rerun()
+    if api_key_box and "rerun" in st.session_state:
         st.session_state["api_key"] = api_key_box
-        if "rerun" in st.session_state:
-            st.experimental_rerun()
+        st.experimental_rerun()
     st.write("GUI bulit by shan-mx")
+
 with st.expander("Statistics"):
     col1, col2, col3 = st.columns(3)
-    col1.metric("Time Used:", chat_stats["Time Used"])
-    col1.metric("Averaged Time:", chat_stats["Averaged Time"])
-    col2.metric("Tokens Used:", chat_stats["Tokens Used"])
-    col2.metric("Total Tokens:", chat_stats["Total Tokens"])
-    col3.metric("Total Rounds:", chat_stats["Total Rounds"])
+    col1.metric("Time Used:", stats["Time Used"])
+    col1.metric("Averaged Time:", stats["Averaged Time"])
+    col2.metric("Tokens Used:", stats["Tokens Used"])
+    col2.metric("Total Tokens:", stats["Total Tokens"])
+    col3.metric("Total Rounds:", stats["Total Rounds"])
+
+with st.expander("Parameters"):
+    temperature_slider = st.slider("Temperature", 0.0, 2.0, float(paras["temperature"]), 0.1)
+    top_p_slider = st.slider("Top P", 0.1, 1.0, float(paras["top_p"]), 0.1)
+    presence_penalty_slider = st.slider("Presence Penalty", -2.0, 2.0, float(paras["presence_penalty"]), 0.1)
+    frequency_penalty_slider = st.slider("Frequency Penalty", -2.0, 2.0, float(paras["frequency_penalty"]), 0.1)
+    if temperature_slider and "rerun" in st.session_state:
+        paras["temperature"] = temperature_slider
+    if top_p_slider and "rerun" in st.session_state:
+        paras["top_p"] = top_p_slider
+    if presence_penalty_slider and "rerun" in st.session_state:
+        paras["presence_penalty"] = presence_penalty_slider
+    if frequency_penalty_slider and "rerun" in st.session_state:
+        paras["frequency_penalty"] = frequency_penalty_slider
--- helper.py
+++ helper.py
@@ -6,23 +6,21 @@ import glob
 import time
 
 
-def save_data(history: list, stats: dict, user: str):
-    with open(f"chat_data_{user}.json", 'w', encoding='utf-8') as f:
-        json.dump({"chat_history": history, "chat_stats": stats}, f)
+def save_data(history: list, stats: dict, paras: dict, user: str):
+    with open(f"{user}.json", 'w', encoding='utf-8') as f:
+        json.dump({"history": history, "stats": stats, "paras": paras}, f)
 
 
 def load_data(user: str, initial_content: str) -> dict:
     try:
-        with open(f"chat_data_{user}.json", 'r', encoding='utf-8') as f:
+        with open(f"{user}.json", 'r', encoding='utf-8') as f:
             data = json.load(f)
             return data
-    except FileNotFoundError:
-        return {"chat_history": [{"role": "system", "content": initial_content}], "chat_stats": initial_stats}
-    except json.decoder.JSONDecodeError:
-        return {"chat_history": [{"role": "system", "content": initial_content}], "chat_stats": initial_stats}
+    except (FileNotFoundError, json.decoder.JSONDecodeError):
+        return initial_data(initial_content)
 
 
-def show_messages(user: str, messages: list, st):
+def show_messages(user: str, messages: list):
     for each in messages:
         if each["role"] == "user":
             st.markdown(f"***\n**{user}:**\n\n{each['content']}")
@@ -31,4 +29,9 @@ def show_messages(user: str, messages: list, st):
     st.markdown("\n")
 
 
+def initial_data(initial_content: str):
+    return {"history": [{"role": "system", "content": initial_content}], "stats": initial_stats, "paras": initial_paras}
+
+
 initial_stats = {"Total Time": 0, "Total Tokens": 0, "Total Rounds": 0, "Time Used": 0, "Tokens Used": 0, "Averaged Time": 0}
+initial_paras = {"temperature": 1, "top_p": 1, "presence_penalty": 0, "frequency_penalty": 0}
