dee3ab99cea3db7d69f7b63aa49e0f5647fe62b5
Alex Shan <36291011+shan-mx@users.noreply.github.com>
2023-03-11 14:59:07 +0800

    debug

--- app.py
+++ app.py
@@ -5,31 +5,50 @@ st.title('GPT Assistant')
 user = "User" if "user" not in st.experimental_get_query_params() else st.experimental_get_query_params()["user"][0]
 initial_content = "" if "initial_content" not in st.session_state else st.session_state["initial_content"]
 api_key = "" if "api_key" not in st.session_state else st.session_state["api_key"]
-
+i = ""
 if "rerun" not in st.session_state:
     st.session_state["rerun"] = True
+
 else:
     st.session_state.pop("rerun")
-
-st.session_state['data'] = load_data(user, initial_content)
+if "data" not in st.session_state:
+    st.session_state['data'] = load_data(user, initial_content)
 history, stats, paras = (value for key, value in st.session_state['data'].items())
+print(paras)
 save_data(history, stats, paras, user)
 
 if user == "User":
     st.markdown("***\nIt seems like you're new to my chatGPT web client! Please create your own user profile in Settings.\n\n")
-if st.session_state["data"]:
-    show_messages(user, st.session_state['data']["history"])
 
+debug = 1
+show_messages(user, st.session_state['data']["history"])
+if "r" in st.session_state and debug:
+    report = ["***\n**Assistant:**\n\n"]
+    res_box = st.empty()
+    print(st.session_state["r"])
+    for e in st.session_state["r"]:
+        if "content" in e["choices"][0]["delta"]:
+            resp = e["choices"][0]["delta"]["content"]
+            report.append(resp)
+            result = "".join(report).strip()
+            res_box.markdown(f'*{result}*')
+    history.append({"role": "assistant", "content": "".join(report).strip().replace('\n', '\n\n')})
+    save_data(history, stats, paras, user)
+    st.session_state.pop("r")
+
+#user_input = st.text_area(f"**{user}:**", key="input", value=i)
+#if 1 == 1:
 with st.form("form", clear_on_submit=True):
     user_input = st.text_area(f"**{user}:**", key="input")
-    if st.form_submit_button("Submit", use_container_width=True):
+    b = st.form_submit_button("Submit", use_container_width=True)
+    if (user_input and "rerun" in st.session_state):
         openai.api_key = api_key if api_key != "" else st.secrets["apikey"]
         stats["Total Rounds"] += 1
         t1 = time.time()
-        history.append({"role": "user", "content": user_input})
+        history.append({"role": "user", "content": user_input.replace('\n', '\n\n')})
         with st.spinner("Processing..."):
             try:
-                r = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=history, **paras)
+                r = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=history, stream =True, **paras)
             except openai.error.APIConnectionError:
                 return_text = "[Warning] API Connection Error, try again."
             except openai.error.InvalidRequestError:
@@ -37,14 +56,15 @@ with st.form("form", clear_on_submit=True):
             except openai.error.RateLimitError:
                 return_text = "[Warning] Reached chat rate limit, do not send request too frequently."
             else:
-                return_text = r["choices"][0]["message"]["content"]
-                stats["Total Tokens"] += r["usage"]["total_tokens"]
-                stats["Tokens Used"] = r["usage"]["total_tokens"]
-            stats["Time Used"] = round(time.time() - t1, 3)
-            stats["Total Time"] += stats["Time Used"]
-            stats["Averaged Time"] = round(stats["Total Time"] / stats["Total Rounds"], 3)
-            history.append({"role": "assistant", "content": return_text})
-            save_data(history, stats, paras, user)
+                st.session_state["r"] = r
+                return_text = "" #r["choices"][0]["message"]["content"]
+                #stats["Total Tokens"] += r["usage"]["total_tokens"]
+                #stats["Tokens Used"] = r["usage"]["total_tokens"]
+            #stats["Time Used"] = round(time.time() - t1, 3)
+            #stats["Total Time"] += stats["Time Used"]
+            #stats["Averaged Time"] = round(stats["Total Time"] / stats["Total Rounds"], 3)
+            #history.append({"role": "assistant", "content": return_text.replace('\n', '\n\n')})
+            #save_data(history, stats, paras, user)
             st.experimental_rerun()
 
 with st.expander("Settings"):
@@ -56,7 +76,7 @@ with st.expander("Settings"):
         user_box = st.selectbox("All Users", [json_file.split("_")[-1].split(".")[0] for json_file in glob.glob(os.getcwd() + '/*.json')])
     else:
         user_box = st.text_input("Username (each user have their own chat data)", value=user)
-    initial_content_box = st.text_input("Initial Instruction (applies when the chat history is cleared)", value=initial_content)
+    initial_content_box = st.text_area("Initial Instruction (applies when the chat history is cleared)", value=initial_content)
     api_key_box = st.text_input("OpenAI API Key (optional)", value=api_key)
     if user_box and "rerun" in st.session_state:
         st.experimental_set_query_params(user=user_box)
@@ -69,7 +89,7 @@ with st.expander("Settings"):
         st.session_state["api_key"] = api_key_box
         if "rerun" in st.session_state:
             st.experimental_rerun()
-    st.write("GUI bulit by shan-mx")
+    st.write("GUI built by shan-mx")
 
 with st.expander("Statistics"):
     col1, col2, col3 = st.columns(3)
